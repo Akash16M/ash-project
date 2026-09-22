116727ec4df42d1dbd54cd5d2ab80248b95e2580
Akash16M <akashmurali2006@gmail.com>
2026-09-22 11:10:32 +0530

    added java file

---
+++ Codechef.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner Sc = new Scanner(System.in);
+		int a = Sc.nextInt();
+		int b = Sc.nextInt();
+		
+		int result = a * b * 100;
+		System.out.println(result);
+
+	}
+}
